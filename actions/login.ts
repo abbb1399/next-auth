fef4d3095f8876734c02d970e09f561b53ffc9e6
paul <abbb123@naver.com>
2024-09-30 15:22:38 +0900

    feat: email resend

--- actions/login.ts
+++ actions/login.ts
@@ -6,6 +6,9 @@ import { signIn } from "@/auth";
 import { LoginSchema } from "@/schemas";
 import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 import { AuthError } from "next-auth";
+import { generateVerificationToken } from "@/lib/tokens";
+import { getUserByEmail } from "@/data/user";
+import { sendVerifictaionEmail } from "@/lib/mail";
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
   const validatedFields = LoginSchema.safeParse(values);
@@ -15,6 +18,22 @@ export const login = async (values: z.infer<typeof LoginSchema>) => {
   }
 
   const { email, password } = validatedFields.data;
+
+  const existingUser = await getUserByEmail(email);
+
+  if (!existingUser || !existingUser.email || !existingUser.password) {
+    return { error: "Email does not exist" };
+  }
+
+  if (!existingUser.emailVerified) {
+    const verficationToken = await generateVerificationToken(
+      existingUser.email
+    );
+
+    await sendVerifictaionEmail(verficationToken.email, verficationToken.token);
+
+    return { success: "Confirmation email sent!" };
+  }
 
   try {
     await signIn("credentials", {
