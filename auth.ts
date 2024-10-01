719316e3241b9a7ce0bf2188a443693e1267dc42
paul <abbb123@naver.com>
2024-10-01 19:42:03 +0900

    feat: Two Factor Authentication

--- auth.ts
+++ auth.ts
@@ -4,6 +4,7 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import { db } from "./lib/db";
 import authConfig from "./auth.config";
 import { getUserById } from "./data/user";
+import { getTwoFactorConfirmationByUserId } from "./data/two-factor-confirmation";
 
 export const {
   handlers: { GET, POST },
@@ -33,7 +34,18 @@ export const {
       // Prevent signin without email verfication
       if (!existingUser?.emailVerified) return false;
 
-      // TODO: add 2fa check
+      if (existingUser.isTwoFactorEnabled) {
+        const twoFactorConfirmation = await getTwoFactorConfirmationByUserId(
+          existingUser.id
+        );
+
+        if (!twoFactorConfirmation) return false;
+
+        // Delete two factor confirmation for next sign in
+        await db.twoFactorConfirmation.delete({
+          where: { id: twoFactorConfirmation.id },
+        });
+      }
 
       return true;
     },
