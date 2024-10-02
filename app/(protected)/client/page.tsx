6a959f2d1e8649a39e5b4e68ba186700dadf991f
paul <abbb123@naver.com>
2024-10-02 16:16:24 +0900

    feat: server & client component example

---
+++ app/(protected)/client/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { useCurrentUser } from "@/app/hooks/use-current-user";
+import { UserInfo } from "@/components/user-info";
+
+const ClientPage = () => {
+  const user = useCurrentUser();
+
+  return <UserInfo label="📱 Client Component" user={user} />;
+};
+
+export default ClientPage;
