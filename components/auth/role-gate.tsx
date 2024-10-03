affe30dda819bffed2b565fdd298a80a88310f78
paul <abbb123@naver.com>
2024-10-03 12:25:39 +0900

    feat: admin exmaple

---
+++ components/auth/role-gate.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { useCurrentRole } from "@/app/hooks/use-current-roles";
+import { UserRole } from "@prisma/client";
+import { FormError } from "../form-error";
+
+interface RoleGateProps {
+  children: React.ReactNode;
+  allowedRole: UserRole;
+}
+
+export const RoleGate = ({ children, allowedRole }: RoleGateProps) => {
+  const role = useCurrentRole();
+
+  if (role !== allowedRole) {
+    return <FormError message="You do not have permission!" />;
+  }
+
+  return <>{children}</>;
+};
