9ef27aed50b14189ab07892a93a95487efaefe25
paul <abbb123@naver.com>
2024-10-04 14:12:29 +0900

    fix: logout to signOut

--- components/auth/logout-button.tsx
+++ components/auth/logout-button.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { logout } from "@/actions/logout";
+// import { logout } from "@/actions/logout";
+import { signOut } from "next-auth/react";
 
 interface LogoutButtonProps {
   children?: React.ReactNode;
@@ -8,7 +9,8 @@ interface LogoutButtonProps {
 
 export const LogoutButton = ({ children }: LogoutButtonProps) => {
   const onClick = () => {
-    logout();
+    // logout()
+    signOut();
   };
 
   return (
